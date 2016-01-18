6c3ad1822a26f50e85e5a3931f116bd68ce441a6
Brian Exelbierd <bexelbie@Brians-MacBook-Pro.local>
2016-01-18 20:34:28 +0100

    Adding OmniOutliner Pro 3.10.6

---
+++ Casks/omnioutliner-3.10.6.rb
@@ -0,0 +1,11 @@
+cask 'omnioutliner-3.10.6' do
+  version '3.10.6'
+  sha256 'ec8ee1ec00870a1cc5920d2e899cd255eef6564c8e9944eadfdf5297f1cb348e'
+
+  url "http://www.omnigroup.com/ftp1/pub/software/MacOSX/10.4/OmniOutlinerPro-#{version}.dmg"
+  name 'OmniOutliner'
+  homepage 'https://www.omnigroup.com/omnioutliner/'
+  license :commercial
+
+  app 'OmniOutliner Professional.app'
+end
---
+++ README.md
@@ -0,0 +1,1 @@
+Some [Homebrew](http://brew.sh) [Casks](http://caskroom.io) I need for my own setup.
